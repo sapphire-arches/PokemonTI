2cfc8417811b07254642e82ffd9a883a8921bf68
Bob Twinkles <srkoser@gmail.com>
2012-03-13 20:07:04 -0400

    Added grass and stuff. We have a problem. Only about a fifth of the screen can be written to without drawing to slow.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,12 @@
 
 main()
 {
-	putsprite (spr_or, 0, 0, spr_tile_grass);
+	char x, y;
+	for (x = 0; x < 94; x+= 8) {
+		for (y = 0; y < 62; y += 8) {
+			if (rand() < RAND_MAX / 5)
+				putsprite (spr_or, x, y, spr_tile_grass);
+		}
+	}
 	getk ();
 }
--- src/spr_tiles.h
+++ src/spr_tiles.h
@@ -1,4 +1,13 @@
-char spr_tile_grass[] = {
-	8, 8
+/*********************************************
+* this is where we save all the tile sprites.*
+*********************************************/
+#ifndef _SPR_TILES_H_INCLUDED
+#define _SPR_TILES_H_INCLUDED
+
+char spr_tile_grass[] = 
+{
+	8, 8,
 	0x4, 0x42, 0x20, 0x0, 0x4, 0x42, 0x20, 0x0
-}
+};
+
+#endif
