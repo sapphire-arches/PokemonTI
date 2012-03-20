b661a7671b93d4c093412c172793c191b1a4af2c
Bob Twinkles <srkoser@gmail.com>
2012-03-20 15:27:32 -0400

    Fudged around trying to get screen clear to work right. Also replaced some tabs with spaces

--- src/ren/renderer.c
+++ src/ren/renderer.c
@@ -1,19 +1,20 @@
 #include "renderer.h"
 
 void ren_draw_map (int xStart, int yStart) {
-	char x,y, mx, my;
-	unsigned char val;
+    char x,y, mx, my;
+    unsigned char val;
 	
-	for (x = 0; x < 94; x += 8) {
-		for (y = 0; y < 62; y += 8) {
-			mx = x / 8;
-			my = y / 8;
-			val = map_map [map_getindex (xStart + mx, yStart + my)];
-			switch (val) {
-				case MAP_GRASS_DEC : putsprite (SPR_OR, x, y, spr_tile_grass); break;
-				case MAP_NONE: putsprite (SPR_AND, x, y, spr_tile_blank); break;
-				default: break;
-			}
-		}
-	}
+    for (x = 0; x < 94; x += 8) {
+        for (y = 0; y < 62; y += 8) {
+            mx = x / 8;
+            my = y / 8;
+            val = map_map [map_getindex (xStart + mx, yStart + my)];
+            putsprite (SPR_XOR, x, y, spr_tile_blank);
+            switch (val) {
+                case MAP_GRASS_DEC : putsprite (SPR_OR, x, y, spr_tile_grass); break;
+                case MAP_NONE: break;
+                default: break;
+            }
+        }
+    }
 }
