44778d6645a50669b5e2ca85ef0ffc045dd30fa9
Bob Twinkles <srkoser@gmail.com>
2012-03-22 11:34:08 -0400

    Did stuff

--- src/main.c
+++ src/main.c
@@ -5,26 +5,28 @@
 #include "main.h"
 
 void init (void) {
-	char x, y;
-	
-	for (x = 0; x < MAP_WIDTH; ++x) {
-		for (y = 0; y < MAP_HEIGHT; ++y) {
-			if (rand () < RAND_MAX / 4) {
-				map_map [map_getindex (x, y)] = MAP_GRASS_DEC;
-			} else {
-				map_map [map_getindex (x, y)] = MAP_NONE;
-			}
-		}
-	}
+    char x, y;
+
+    for (x = 0; x < MAP_WIDTH; ++x) {
+        for (y = 0; y < MAP_HEIGHT; ++y) {
+            if (rand () < RAND_MAX / 4) {
+                map_map [map_getindex (x, y)] = MAP_GRASS_DEC;
+            } else {
+                map_map [map_getindex (x, y)] = MAP_NONE;
+            }
+        }
+    }
 }
 
-void main()
-{	
-	putsprite (SPR_OR, 10, 10, spr_wait);
-	
-	init ();
-	
-	ren_draw_map (0, 0);
-	
-	getk ();
+void main() {
+    putsprite (SPR_OR, 10, 10, spr_wait);
+
+    init ();
+
+    ren_draw_map (0, 0);
+
+    while (1) {
+        
+    }
+    getk ();
 }
