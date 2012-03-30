9d01115ded09f5617f7fa606e20adea05d690bec
Bob Twinkles <srkoser@gmail.com>
2012-03-30 13:42:40 -0400

    Removed trailing whitespace.

--- src/spr_tiles.h
+++ src/spr_tiles.h
@@ -4,13 +4,13 @@
 #ifndef _SPR_TILES_H_INCLUDED
 #define _SPR_TILES_H_INCLUDED
 
-char spr_tile_grass[] = 
+char spr_tile_grass[] =
 {
 	8, 8,
 	0x4, 0x42, 0x20, 0x0, 0x4, 0x42, 0x20, 0x0
 };
 
-char spr_tile_blank[] = 
+char spr_tile_blank[] =
 {
 	8, 8,
 	0, 0, 0, 0, 0, 0, 0, 0
